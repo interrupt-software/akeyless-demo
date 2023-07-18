99caf9e338f99d74f1128df87560a63c73412ce8
gcastill0 <gilberto@hashicorp.com>
2023-07-18 08:32:01 -0400

    Adding slides

---
+++ docs/js/paths.js
@@ -0,0 +1,240 @@
+var jsondata = {
+    "about-this-exercise": [
+        {
+            "id": 0,
+            "class": "cell-span-5-5",
+            "splash_delay": 3,
+            "image": "headshot.svg"
+        },
+        {
+            "id": 2,
+            "class": "cell-span-6-5",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "../html/templates/p01_t01.html",
+                "template_class": "cell-content-normal"
+            }
+        }
+    ],
+    "track-contents": [
+        {
+            "id": 0,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "label": {
+                "text": "Authorization",
+                "class": "cell-label-top"
+            },
+            "boxtype": "cell-decorated",
+            "innerHTML": {
+                "template": "../html/templates/p04_t01.html",
+                "template_class": "cell-content-small"
+            }
+        },
+        {
+            "id": 14,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "image": "container-3-2-1.svg"
+        },
+        {
+            "id": 15,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "image": "container-3-2-2.svg"
+        },
+        {
+            "id": 16,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "image": "container-3-2-3.svg"
+        },
+        {
+            "id": 17,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "image": "container-3-2-4.svg"
+        },
+        {
+            "id": 4,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "label": {
+                "text": "Private Networks",
+                "class": "cell-label-top"
+            },
+            "boxtype": "cell-decorated",
+            "innerHTML": {
+                "template": "../html/templates/p04_t03.html",
+                "template_class": "cell-content-small"
+            }
+        },
+        {
+            "id": 21,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "label": {
+                "text": "Secrets",
+                "class": "cell-label-top"
+            },
+            "boxtype": "cell-decorated",
+            "innerHTML": {
+                "template": "../html/templates/p04_t02.html",
+                "template_class": "cell-content-small"
+            }
+        },
+        {
+            "id": 25,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "label": {
+                "text": "Dynamic Secrets",
+                "class": "cell-label-top"
+            },
+            "boxtype": "cell-decorated",
+            "innerHTML": {
+                "template": "../html/templates/p04_t04.html",
+                "template_class": "cell-content-small"
+            }
+        }
+    ],
+    "secrets-orchestration-auth": [
+        {
+            "id": 0,
+            "class": "cell-span-5-5",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "../html/templates/p02_t01.html",
+                "template_class": "cell-content-normal"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-5-5",
+            "splash_delay": 3,
+            "image": "authentication_and_authorization.svg"
+        }
+    ],
+    "secrets-orchestration-fetch": [
+        {
+            "id": 0,
+            "class": "cell-span-5-5",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "../html/templates/p02_t02.html",
+                "template_class": "cell-content-normal"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-5-5",
+            "splash_delay": 3,
+            "image": "secrets_access.svg"
+        }
+    ],
+    "secrets-orchestration-gateway": [
+        {
+            "id": 0,
+            "class": "cell-span-5-5",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "../html/templates/p02_t03.html",
+                "template_class": "cell-content-normal"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-5-5",
+            "splash_delay": 3,
+            "image": "akeyless_gateway.svg"
+        }
+    ],
+    "industry-challenges": [
+        {
+            "id": 0,
+            "class": "cell-span-12-6",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "../html/templates/p98_t01.html",
+                "template_class": "cell-content-normal"
+            }
+        }
+    ],
+    "application-secrets": [
+        {
+            "id": 0,
+            "class": "cell-span-1-6",
+            "label": {
+                "text": "Application Secrets",
+                "class": "cell-label-left"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-12-5",
+            "splash_delay": 3,
+            "image": "application_solution.svg"
+        }
+    ],
+    "application-environment": [
+        {
+            "id": 0,
+            "class": "cell-span-1-6",
+            "label": {
+                "text": "Application Environment",
+                "class": "cell-label-left"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-12-5",
+            "splash_delay": 3,
+            "image": "application_baseline.svg"
+        }
+    ],
+    "application-challenges": [
+        {
+            "id": 0,
+            "class": "cell-span-1-6",
+            "label": {
+                "text": "Application Challenges",
+                "class": "cell-label-left"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-12-5",
+            "splash_delay": 3,
+            "image": "application_challenges.svg"
+        }
+    ],
+    "cicd-enablement": [
+        {
+            "id": 0,
+            "class": "cell-span-1-6",
+            "label": {
+                "text": "Kubernetes Options",
+                "class": "cell-label-left"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-12-5",
+            "splash_delay": 3,
+            "image": "akeyless_on_kubernetes.svg"
+        }
+    ],
+    "summarize-benefits": [
+        {
+            "id": 0,
+            "class": "cell-span-12-6",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "../html/templates/p99_t01.html",
+                "template_class": "cell-content-normal"
+            }
+        }
+    ]
+};
+
+export { jsondata };
